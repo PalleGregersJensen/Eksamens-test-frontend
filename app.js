1685bcacc85e9a50bb653488fc4fcb41d619fef0
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-11-02 14:04:15 +0100

    Data vises på hjemmeside

--- app.js
+++ app.js
@@ -4,8 +4,18 @@ import { getVacantShifts } from "./rest-api.js";
 
 window.addEventListener("load", start);
 
+// Get data from backend
 async function start() {
     console.log("App is runnning");
     const vacantShifts = await getVacantShifts();
     console.log(vacantShifts);
+    showDataOnWebsite(vacantShifts);
+}
+
+// Display data on website
+function showDataOnWebsite(shiftsList) {
+  for (const shift of shiftsList) {
+    const vacantShiftsHtml = /*html*/ `${shift.Year}-${shift.Date}-${shift.Time}-${shift.LengthOfShift}`;
+    document.querySelector("#vacant-shifts-container").insertAdjacentHTML("beforeend", vacantShiftsHtml);
+  }
 }
