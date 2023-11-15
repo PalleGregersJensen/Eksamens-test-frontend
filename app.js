884f3f399c9f79867d30cb56ac2c3972e1ea6388
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-11-15 13:53:23 +0100

    Udkommenterede dele er slettet. Der kan klikkes på den specifikke vagt, og den kommer frem i konsollen. Logik til booking af specifik vagt mangler.

--- app.js
+++ app.js
@@ -14,92 +14,34 @@ async function start() {
   console.log("App is runnning");
   const vacantShifts = await getVacantShifts();
   console.log(vacantShifts);
-  // showDataOnWebsite(vacantShifts);
   employees = await getEmployees();
   console.log(employees);
-  // insertEmployeesIntoDatalist(employees);
   shift = new Shift();
   shift.showDataOnWebsite(vacantShifts);
-  // let shift1 = new Shift();
-  // shift1.vacantShiftClicked(vacantShifts);
-  // shift.vacantShiftClicked();
-  document
-        .querySelector("#vacant-shifts-container div:last-child")
-        .addEventListener("click", (shift) => vacantShiftClicked(vacantShifts));
 }
 
-function vacantShiftClicked(shiftObject) {
-  const newShift = new Shift();
-  newShift.vacantShiftClicked(shiftObject);
-}
-
-// Display data on website
-// function showDataOnWebsite(shiftsList) {
-//   document.querySelector("#vacant-shifts-container").innerHTML = "";
-//   for (const shift of shiftsList) {
-//     const isoDateString = shift.Date;
-//     const date = new Date(isoDateString);
-//     // Hvis EmployeeID er null
-//     if (shift.EmployeeID === null) {
-//       shift.EmployeeID = "Ingen";
-//     }
-//     const vacantShiftsHtml = /*html*/ `<div id=${isoDateString}>Dato for vagt: <br> ${date}. <br> Start på vagt: <br> ${shift.Time}. <br> Længde af vagt: <br> ${shift.LengthOfShift} <br> Denne vagt er taget af: <br> ${shift.EmployeeID}</div>`;
-//     document.querySelector("#vacant-shifts-container").insertAdjacentHTML("beforeend", vacantShiftsHtml);
-
-//     document
-//       .querySelector("#vacant-shifts-container div:last-child")
-//       .addEventListener("click", () => vacantShiftClicked(shift));
-//   }
-// }
-
-// vacant shift clicked
-// function vacantShiftClicked(shiftObject) {
-//   console.log(shiftObject);
-//   shift.insertEmployeesIntoDatalist(employees);
-//   console.log(employees);
-//   document.querySelector("#book-shift-dialog").showModal();
-
-  // insert employees array into datalist
-  // function insertEmployeesIntoDatalist(employeesList) {
-  //   console.log(employeesList);
-  //   document.querySelector("#book-shift-datalist").innerHTML = "";
-  //   for (const employee of employeesList) {
-  //     const employeeHtml = /*html*/ `<option value = "${employee.FirstName} ${employee.LastName}">`;
-  //     document.querySelector("#book-shift-datalist").insertAdjacentHTML("beforeend", employeeHtml);
-  //     console.log(employeeHtml);
-
-  //     document.querySelector("#book-shift-form").addEventListener("submit", bookSpecificShift);
-  //   }
-  //   // document.querySelector("#book-shift-form").addEventListener("submit", bookSpecificShift);
-  // }
-
-  // book specific shift (update functionality)
-  async function bookSpecificShift(employeeObject) {
-    event.preventDefault();
-    console.log("book shift");
-    const form = event.target;
-    const fullName = form.employee.value;
-    console.log(fullName);
-    const foundEmployee = employees.find((employee) => `${employee.FirstName} ${employee.LastName}` === fullName);
-    console.log(foundEmployee);
-    form.reset();
-    const employeeAsJson = JSON.stringify(foundEmployee);
-    console.log(employeeAsJson);
-    const response = await fetch(`${endpoint}/vacant_shifts/${shiftObject.ShiftID}`, {
-      method: "PUT",
-      body: employeeAsJson,
-      headers: {
-        "content-Type": "application/json",
-        "credentials": "include",
-      },
-    });
-    if (response.ok) {
-      // if success, run start
-      start();
-    }
+// book specific shift (update functionality)
+async function bookSpecificShift(employeeObject) {
+  event.preventDefault();
+  console.log("book shift");
+  const form = event.target;
+  const fullName = form.employee.value;
+  console.log(fullName);
+  const foundEmployee = employees.find((employee) => `${employee.FirstName} ${employee.LastName}` === fullName);
+  console.log(foundEmployee);
+  form.reset();
+  const employeeAsJson = JSON.stringify(foundEmployee);
+  console.log(employeeAsJson);
+  const response = await fetch(`${endpoint}/vacant_shifts/${shiftObject.ShiftID}`, {
+    method: "PUT",
+    body: employeeAsJson,
+    headers: {
+      "content-Type": "application/json",
+      credentials: "include",
+    },
+  });
+  if (response.ok) {
+    // if success, run start
+    start();
   }
-
-
-
-
-
+}
--- shift.js
+++ shift.js
@@ -1,47 +1,51 @@
 export default class Shift {
-    constructor(Date, Time, LengthOfShift, EmployeeID) {
-        this.Date = Date;
-        this.Time = Time;
-        this.LengthOfShift = LengthOfShift;
-        this.EmployeeID = EmployeeID;
-    }
-    // Display data on website
-    showDataOnWebsite(shiftsList) {
-        document.querySelector("#vacant-shifts-container").innerHTML = "";
-        for (const shift of shiftsList) {
-            const isoDateString = shift.Date;
-            const date = new Date(isoDateString);
-            // Hvis EmployeeID er null
-            if (shift.EmployeeID === null) {
-                shift.EmployeeID = "Ingen";
-            }
-            const vacantShiftsHtml = /*html*/ `<div id=${isoDateString}>Dato for vagt: <br> ${date}. <br> Start på vagt: <br> ${shift.Time}. <br> Længde af vagt: <br> ${shift.LengthOfShift} <br> Denne vagt er taget af: <br> ${shift.EmployeeID}</div>`;
-            document.querySelector("#vacant-shifts-container").insertAdjacentHTML("beforeend", vacantShiftsHtml);
+  constructor(Date, Time, LengthOfShift, EmployeeID) {
+    this.Date = Date;
+    this.Time = Time;
+    this.LengthOfShift = LengthOfShift;
+    this.EmployeeID = EmployeeID;
+  }
+  // Display data on website
+  showDataOnWebsite(shiftsList) {
+    document.querySelector("#vacant-shifts-container").innerHTML = "";
+    for (const shift of shiftsList) {
+      const isoDateString = shift.Date;
+      const date = new Date(isoDateString);
+      // Hvis EmployeeID er null
+      if (shift.EmployeeID === null) {
+        shift.EmployeeID = "Ingen";
+      }
+      const vacantShiftsHtml = /*html*/ `<div id=${isoDateString}>Dato for vagt: <br> ${date}. <br> Start på vagt: <br> ${shift.Time}. <br> Længde af vagt: <br> ${shift.LengthOfShift} <br> Denne vagt er taget af: <br> ${shift.EmployeeID}</div>`;
+      document.querySelector("#vacant-shifts-container").insertAdjacentHTML("beforeend", vacantShiftsHtml);
 
-            // document
-            //     .querySelector("#vacant-shifts-container div:last-child")
-            //     .addEventListener("click", () => vacantShiftClicked(shift));
-        }
+      document
+        .querySelector("#vacant-shifts-container div:last-child")
+        .addEventListener("click", () => this.vacantShiftClicked(shift));
     }
-    insertEmployeesIntoDatalist(employeesList) {
-        console.log(employeesList);
-        document.querySelector("#book-shift-datalist").innerHTML = "";
-        for (const employee of employeesList) {
-            const employeeHtml = /*html*/ `<option value = "${employee.FirstName} ${employee.LastName}">`;
-            document.querySelector("#book-shift-datalist").insertAdjacentHTML("beforeend", employeeHtml);
-            console.log(employeeHtml);
+  }
+  insertEmployeesIntoDatalist(employeesList) {
+    console.log(employeesList);
+    document.querySelector("#book-shift-datalist").innerHTML = "";
+    for (const employee of employeesList) {
+      const employeeHtml = /*html*/ `<option value = "${employee.FirstName} ${employee.LastName}">`;
+      document.querySelector("#book-shift-datalist").insertAdjacentHTML("beforeend", employeeHtml);
+      console.log(employeeHtml);
 
-            //   document.querySelector("#book-shift-form").addEventListener("submit", bookSpecificShift);
-        }
-        // document.querySelector("#book-shift-form").addEventListener("submit", bookSpecificShift);
+      document.querySelector("#book-shift-form").addEventListener("submit", bookSpecificShift);
     }
-        // vacant shift clicked
-        vacantShiftClicked(shiftObject) {
-            console.log(shiftObject);
-            // shift.insertEmployeesIntoDatalist(employees);
-            // console.log(employees);
-            document.querySelector("#book-shift-dialog").showModal();
-    
-        }
+    document.querySelector("#book-shift-form").addEventListener("submit", bookSpecificShift);
+  }
+  // vacant shift clicked
+  vacantShiftClicked(shiftObject) {
+    console.log(shiftObject);
+    document.querySelector("#book-shift-dialog").showModal();
+  }
 
-    }
+  // vacantShiftClicked(shiftObject) {
+  //     console.log(shiftObject);
+  //     // shift.insertEmployeesIntoDatalist(employees);
+  //     // console.log(employees);
+  //     document.querySelector("#book-shift-dialog").showModal();
+
+  // }
+}
